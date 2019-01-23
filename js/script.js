5b0a6eb554feaf52fbbdaca83afebb5a734a719a
Mashustia <denisova.mariya.dm@gmail.com>
2019-01-24 01:08:19 +0300

    Исправлено поведение кнопки 2

--- js/script.js
+++ js/script.js
@@ -12,6 +12,20 @@
     centerMode: true,
     arrows: true,
     lazyLoad: 'progressive',
-    variableWidth: true
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1365,
+        settings: {
+          infinite: true,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          centerMode: true,
+          arrows: false,
+          lazyLoad: 'progressive',
+          variableWidth: true
+        }
+      }
+    ]
   });
 }) ();
